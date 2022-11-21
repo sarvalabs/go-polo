eef157e7b084e65f0e474693abd1be19c989b19f
Manish Meganathan <manish@sarva.ai>
2022-11-21 19:52:00 +0530

    Implement Decode Buffer Types
    - Added readbuffer and loadreader types for read-only bytes handling while decoding
    - Added a custom error MalformedTagError for handling malformed varint tags encountered while decoding

--- errors.go
+++ errors.go
@@ -1,9 +1,22 @@
 package polo
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 // varint errors
 var (
 	errVarintTerminated = errors.New("varint terminated prematurely")
 	errVarintOverflow   = errors.New("varint overflows 64-bit integer")
 )
+
+// MalformedTagError is an error for when a consumed varint for a tag is malformed
+type MalformedTagError struct {
+	msg string
+}
+
+// Error implements the error interface for MalformedTagError
+func (err MalformedTagError) Error() string {
+	return fmt.Sprintf("malformed tag: %v", err.msg)
+}
