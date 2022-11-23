5119f678bd9c9ed4013dfef4f38451bd1b4ce58f
Manish Meganathan <manish@sarva.ai>
2022-11-24 01:10:14 +0530

    Add Document Get/Set Functionality for Objects
    - Added GetObject and SetObject methods for Document. They can be used to directly get and set objects with a string key from Document. (as specified in #2)
    - Added tests for both methods in document_test.go

--- document_test.go
+++ document_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestDocument_Bytes(t *testing.T) {
@@ -70,4 +71,59 @@ func TestDocument_GetSet(t *testing.T) {
 	// Attempt to retrieve some set keys from the document and confirm equality
 	assert.Equal(t, []byte{1, 0, 1, 0}, doc.Get("foo"))
 	assert.Equal(t, []byte{2, 1, 2, 1}, doc.Get("bar"))
+}
+
+func TestDocument_SetObject(t *testing.T) {
+	// Create a Document
+	var err error
+	doc := make(Document)
+
+	// Set some objects into the document and confirm nil errors
+	err = doc.SetObject("foo", 25)
+	require.Nil(t, err)
+	err = doc.SetObject("bar", "hello")
+	require.Nil(t, err)
+
+	// Get the raw data for the keys from the document and confirm equality
+	assert.Equal(t, []byte{3, 25}, doc.Get("foo"))
+	assert.Equal(t, []byte{6, 104, 101, 108, 108, 111}, doc.Get("bar"))
+
+	err = doc.SetObject("far", make(chan int))
+	assert.EqualError(t, err, "document value could not be encoded for key 'far': unsupported type: chan int [chan]")
+}
+
+func TestDocument_GetObject(t *testing.T) {
+	// Create a Document
+	var err error
+	doc := make(Document)
+
+	// Set some objects into the document and confirm nil errors
+	err = doc.SetObject("foo", 25)
+	require.Nil(t, err)
+	err = doc.SetObject("bar", "hello")
+	require.Nil(t, err)
+
+	// Attempt to retrieve the integer object from the Document.
+	// Test for nil error and value equality
+	var foo int
+	err = doc.GetObject("foo", &foo)
+	assert.Nil(t, err)
+	assert.Equal(t, 25, foo)
+
+	// Attempt to retrieve the string object from the Document.
+	// Test for nil error and value equality
+	var bar string
+	err = doc.GetObject("bar", &bar)
+	assert.Nil(t, err)
+	assert.Equal(t, "hello", bar)
+
+	// Attempt to retrieve object from non-existent field from the Document.
+	// Tests error string
+	err = doc.GetObject("far", &foo)
+	assert.EqualError(t, err, "document value not found for key 'far'")
+
+	// Attempt to retrieve object from a field with wrong type from Document.
+	// Test error string
+	err = doc.GetObject("bar", &foo)
+	assert.EqualError(t, err, "document value could not be decoded for key 'bar': decode error: incompatible wire type. expected: posint. got: word")
 }
