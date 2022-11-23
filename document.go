a8e9187db2b8f39b047fdb587018286090aae0a0
Manish Meganathan <manish@sarva.ai>
2022-11-24 01:10:14 +0530

    Add Document Get/Set Functionality for Objects
    - Added GetObject and SetObject methods for Document. They can be used to directly get and set objects with a string key from Document. (as specified in #2)
    - Added tests for both methods in document_test.go

--- document.go
+++ document.go
@@ -1,5 +1,9 @@
 package polo
 
+import (
+	"fmt"
+)
+
 // Document is a representation for a string indexed collection of encoded object data.
 // It represents an intermediary access format with objects settable/gettable with string keys.
 type Document map[string][]byte
@@ -36,4 +40,37 @@ func (doc Document) Is(key string, kind WireType) bool {
 	}
 
 	return data[0] == byte(kind)
+}
+
+// GetObject retrieves some object for a given from a Document.
+// The data for the given key is decoded from its POLO form into the given object which must be a pointer.
+// Returns an error if there is no data for the key or if the data could not be decoded into the given object.
+func (doc Document) GetObject(key string, object any) error {
+	// Retrieve the data for the key and error if unavailable
+	var data []byte
+	if data = doc[key]; data == nil {
+		return fmt.Errorf("document value not found for key '%v'", key)
+	}
+
+	// Depolorize the data into the given object and return any error
+	if err := Depolorize(object, data); err != nil {
+		return fmt.Errorf("document value could not be decoded for key '%v': %w", key, err)
+	}
+
+	return nil
+}
+
+// SetObject inserts some object for a given key into a Document.
+// The given object is encoded into its POLO form and inserted, overwriting any existing data.
+// Returns an error if the given object cannot be serialized with Polorize().
+func (doc Document) SetObject(key string, object any) error {
+	// Polorize the object into its wire form. Return any error that occurs
+	data, err := Polorize(object)
+	if err != nil {
+		return fmt.Errorf("document value could not be encoded for key '%v': %w", key, err)
+	}
+
+	// Insert the wire data for the key
+	doc.Set(key, data)
+	return nil
 }
