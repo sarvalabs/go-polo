5fd962aac30d7a6a38b5765263bb8a60d4f35777
Manish Meganathan <manish@sarva.ai>
2022-11-29 09:56:52 +0530

    Add Document Encode/Decode Benchmarks

--- bench_test.go
+++ bench_test.go
@@ -36,3 +36,37 @@ func BenchmarkMixed(b *testing.B) {
 		}
 	})
 }
+
+func BenchmarkDocument(b *testing.B) {
+	object := MixedObject{
+		A: "Sins & Virtues",
+		B: 567822,
+		C: []string{"pride", "greed", "lust", "gluttony", "envy", "wrath", "sloth"},
+		D: map[string]string{"bravery": "piety", "friendship": "chastity"},
+		E: 45.23,
+	}
+
+	docwire, _ := DocumentEncode(object)
+	newObject := new(MixedObject)
+	newDocument := make(Document)
+
+	b.Run("Doc Encode", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_, _ = DocumentEncode(object)
+		}
+	})
+
+	b.Run("Doc Decode", func(b *testing.B) {
+		b.Run("Struct", func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				_ = Depolorize(newObject, docwire)
+			}
+		})
+
+		b.Run("Document", func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				_ = Depolorize(&newDocument, docwire)
+			}
+		})
+	})
+}
